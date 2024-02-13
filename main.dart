72ddb675fbb6138f470903772ea3f31c7f1abfad
Haneena* <haneenaajmi456*>
2024-02-13 15:52:52 +0530

    ConterApplication

---
+++ main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter Application',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  ValueNotifier<int> _counter = ValueNotifier(0);
+
+  void _incrementCounter() {
+    _counter.value = _counter.value + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            ValueListenableBuilder(
+                valueListenable: _counter,
+                builder: (BuildContext ctx, int newValue, Widget? child) {
+                  return Text(
+                    '$newValue',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
